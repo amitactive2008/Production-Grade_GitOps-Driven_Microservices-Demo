2072cbc60afceef564bb2a36c84900e78d6b2956
Amit Kumar <amit.active2008@gmail.com>
2026-08-11 09:18:08 +0530

    .

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -53,9 +53,7 @@ module "eks" {
 
   eks_managed_node_groups = {
     example = {
-      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["c7i-flex.large"]
+      instance_types = ["c7i-flex.xlarge"]
 
       min_size     = 2
       max_size     = 10
